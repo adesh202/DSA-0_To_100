5f770e6cf3c263e33b3b2dc0fb5ff42cba4c0dee
adesh-devops <adeshkumar2501@gmail.com>
2023-11-18 13:35:06 +0530

    Mirror Number

---
+++ Day_01/NumberMirror.py
@@ -0,0 +1,7 @@
+#Write a program that takes a number N as the input, and prints it to the output
+
+# Taking input from the user
+N = int(input("Enter Number : "))
+
+# Printing the input number
+print("Mirror Number is :",N)
