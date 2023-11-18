267593dbf3063b17d0584716b4188acf17ba8890
adesh202 <adeshkumar2501@gmail.com>
2023-11-18 14:43:27 +0530

    double rent

---
+++ Day_01/DoubleRent.py
@@ -0,0 +1,10 @@
+#Chefina decided to move into Chef's apartment. Chef was initially paying a rent of X rupees. Since Chefina is moving in, the owner decided to double the rent. Find the final rent Chef needs to pay
+
+# Input: Initial rent (X)
+X = int(input("Enter the initial rent: "))
+
+# Calculate the final rent
+Y = 2 * X
+
+# Output the result
+print("The final rent is :", Y)
