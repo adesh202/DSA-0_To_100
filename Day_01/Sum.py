0c8cf95b4488fe9a0aaa9c95d2144bb2df139320
adesh202 <adeshkumar2501@gmail.com>
2023-11-20 11:28:29 +0530

    Add Two Numbers

---
+++ Day_01/Sum.py
@@ -0,0 +1,5 @@
+# Your task is very simple: given two integers A and B, write a program to add these two numbers and output the sum
+# Input: Number of test cases
+for i in range (int(input())):
+    N, M = map(int, input().split())
+    print(N+M)
