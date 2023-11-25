49187134ad6dd5a91c835449829d389eddd7ec8b
adesh202 <adeshkumar2501@gmail.com>
2023-11-25 20:01:36 +0530

    String Game

---
+++ Day_03/StringGame.py
@@ -0,0 +1,11 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    s = str(input())
+    
+    for i in set(s):
+        if s.count(i)%2 != 0:
+            print('NO')
+            break
+    else:
+        print('YES')
