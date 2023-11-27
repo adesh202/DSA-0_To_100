ed76a710f3f3572e571257b84d71fba60acae09d
adesh202 <adeshkumar2501@gmail.com>
2023-11-27 19:44:27 +0530

    Is Array Is Sorted Or Not

---
+++ Day_05/IsArraySorted.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        for i in range (len(nums)):
+            if nums == sorted(nums):
+                return True
+            nums= nums[1 : ] + [nums[0]]
+        return False
