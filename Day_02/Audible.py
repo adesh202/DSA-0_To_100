a74b4d086fce25b00959df851d2e3d203e39b151
adesh202 <adeshkumar2501@gmail.com>
2023-11-29 15:36:54 +0530

    Audible Range

---
+++ Day_02/Audible.py
@@ -0,0 +1,9 @@
+# https://www.codechef.com/problems/AUDIBLE?tab=statement
+
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if(x>=67 and x<=45000):
+        print("YES")
+    else:
+        print("NO")
